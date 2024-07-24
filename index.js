d4103c2db5f9a5e260463c0e237b0a8192820d0f
jaodograu444 <133382263+jaodograu444@users.noreply.github.com>
2024-07-24 10:55:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const nome = "joao pereira";
+const idade = 16;
+const rua  = "Rua Anneliese Gellert Krigsnet 3202";
+const Bairro = "Bairro iná";
+const Apartamento = "bloco 7 ap 17";
+
+console.log(nome);
+console.log(idade);
+console.log(rua);
+console.log(Bairro);
+console.log(Apartamento)
